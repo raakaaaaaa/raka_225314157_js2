35fabda9a6c40618a36ec16fc73bb0199481bc26
raakaaaaaa <160812513+raakaaaaaa@users.noreply.github.com>
2024-03-14 12:17:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    let expression = document.getElementById('display').value;
+    let result = eval(expression);
+    document.getElementById('display').value = result;
+  }
